222f631d3de6c391d221ead7937f87da8218fc34
RioNomura <135801386+RioNomura@users.noreply.github.com>
2024-07-06 15:13:21 +0900

    Add files via upload

---
+++ Okashi/OkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiApp.swift
+//  Okashi
+//
+//  Created by 野村龍王 on 2024/06/13.
+//
+
+import SwiftUI
+
+@main
+struct OkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
